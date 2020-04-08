a9ba3a19e3566d8e2d3822f7b0b7b1c4f7a20306
carlosmgc2003 <carlosmgc2003@gmail.com>
2020-04-08 15:10:03 -0300

    Agregadas aclaraciones al codigo, para aprender, claro

--- busqueda_sin_dominio/primero_profundidad.go
+++ busqueda_sin_dominio/primero_profundidad.go
@@ -3,12 +3,13 @@ package busqueda_sin_dominio
 import "container/list"
 
 func Primero_profundidad(raiz *Nodo, buscado string) []string {
+	//Algoritmo de primero en profundidad, el problema por ahora es que destruye el grafo.
 	solucion := make([]string, 0)
 	pila := list.New()
 	pila.PushFront(raiz)
 	for pila.Len() > 0 {
 		visitado := pila.Front()
-		var hijo interface{}
+		var hijo interface{} //La variable hijo tiene que ser de tipo interface porque viene "Encapsulada" del contenedor
 		if len(visitado.Value.(*Nodo).Hijos) > 0 {
 			hijo = visitado.Value.(*Nodo).Hijos[0]
 		}
@@ -18,11 +19,12 @@ func Primero_profundidad(raiz *Nodo, buscado string) []string {
 		} else {
 			if hijo == nil {
 				pila.Remove(visitado)
-				solucion = solucion[:len(solucion)-1]
+				solucion = solucion[:len(solucion)-1] //Slice para eliminar la ultima solucion que no funcionó (backtrack)
 			} else {
 				solucion = append(solucion, visitado.Value.(*Nodo).Dato)
 				pila.PushFront(hijo)
-				visitado.Value.(*Nodo).Hijos = visitado.Value.(*Nodo).Hijos[1:]
+				visitado.Value.(*Nodo).Hijos = visitado.Value.(*Nodo).Hijos[1:] //Elimino al primero puntero hijo que no
+				//me sirvio... esto destruye el arbol por ahora es la unica manera que lo hago.
 			}
 		}
 	}
